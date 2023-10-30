8241a061b3c3d23e0d4b09318eeb292a6607b363
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-10-30 23:18:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by dappa\nhttps://t.me/mahadappa")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by sahil\nhttps://t.me/Its_Tartaglia_Childe")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
